a478721f2a6674a3992bc4954c60ec5aad0125fc
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-01-13 22:04:50 +0100

    fix: update file paths and improve HTML structure for member pages

--- login/auth-guard.js
+++ login/auth-guard.js
@@ -1,14 +1,14 @@
-import { auth } from "/join/firebase.js";
+import { auth } from "../scripts/firebase/firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
-    if (!user) {
-        window.location.href = "index.html";
-    }
-
-    if (user.isAnonymous) {
-        console.log("User ist als Gast eingeloggt");
-    } else {
-        console.log("User ist als Member eingeloggt");
-    }
+  if (!user) {
+    window.location.href = "index.html";
+  }
+  // Nachdem alles fertig ist, kann die untere if Anweisung entfernt werden.
+  if (user.isAnonymous) {
+    console.log("User ist als Gast eingeloggt");
+  } else {
+    console.log("User ist als Member eingeloggt");
+  }
 });
