e79786bb94f0f4614aadc99b69524ef01e6d67de
Zein168 <zein.9494@outlook.com>
2026-03-09 07:08:57 +0100

    feat: add doneTasks function

--- member/js/summary.js
+++ member/js/summary.js
@@ -2,13 +2,22 @@ import { loadTasks } from '/scripts/firebase/get-firebase.js';
 
 
 async function initSummary() {
-    const tasks = await loadTasks();   
+    const tasks = await loadTasks();
+    doneTasks(tasks);   
     todoTasks(tasks);              
 }
 
+// [ ] Show how much tasks "todo"
 function todoTasks(tasks) {
     const count = tasks.filter(task => task.status === "todo").length;
     document.getElementById("todo-count").textContent = count;
 }
+
+// [ ] show how much tasks "done"
+async function doneTasks(tasks) {
+    const count = tasks.filter(task => task.status === "done").length;
+    document.getElementById("done-count").textContent = count;
+}
+
 
 initSummary();
